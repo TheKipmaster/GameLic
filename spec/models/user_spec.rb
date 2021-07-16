6929aca9c7d8137158f5f74ce01fb71126852fd8
TheKipmaster <fkipman@hotmail.com>
2021-07-16 18:12:21 -0300

    Implements user model unit tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "types" do
+    before do
+      @user = User.new(
+        name: "Felippe",
+        institution: "UnB",
+        nickname: "F.",
+        age: "23",
+        email: "fkipman@hotmail.com",
+        password: "123456"
+      )
+      @saved = @user.save
+    end
+
+    context "valid user" do
+      it "is saved" do
+        expect(@saved).to eq(true)
+      end
+
+      it "is student by default" do
+        expect(@user.student?).to eq(true)
+      end
+    end
+
+    context "promoted user" do
+      it "is now a narrator" do
+        @user.type = "Narrator"
+        @saved = @user.save
+
+        expect(@saved).to eq(true)
+        expect(@user.narrator?).to eq(true)
+      end
+    end
+  end
 end
