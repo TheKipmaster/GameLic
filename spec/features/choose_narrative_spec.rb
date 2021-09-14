65901dc32ba8cec8a37922476301de708c406f8c
TheKipmaster <fkipman@hotmail.com>
2021-09-14 16:15:15 -0300

    Corrects 'choose_narrative_spec'

--- spec/features/choose_narrative_spec.rb
+++ spec/features/choose_narrative_spec.rb
@@ -1,25 +1,35 @@
 feature "Choose narrative" do
   background do
+    @narrator = User.create(
+      email: "vitor@example.com",
+      password: "caplin123",
+      name: "vitor",
+      type: "Narrator"
+    )
+    @narrator.confirm
+
     @user = User.create(
       email: "joao@example.com",
       password: "caplin",
       name: "joao"
     )
     @user.confirm
-    login_as(@user, :scope => :user)
+    login_as @user
 
     @narrative = Narrative.create(
       title: "Título",
       description: "Lorem",
       size: 5,
+      narrator: @narrator
     )
     Narrative.toggle_open
   end
 
   scenario "with student logged in" do
+    byebug
     visit "/narratives"
-    click_link("Visualizar")
-    click_link("Escolher essa narrativa")
+    click_link("Título")
+    click_link("Inscrever-se na mesa")
     expect(page).to have_content @user.name
   end
 end
