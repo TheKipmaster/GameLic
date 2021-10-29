9ed3e06cd2ca62762e1f137522d7f88473fb9f37
TheKipmaster <fkipman@hotmail.com>
2021-10-29 18:30:01 -0300

    Translates sign-up notices to portuguese and refactors affected tests accordingly

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -8,16 +8,16 @@ feature "Signing up" do
   scenario "with unused credentials" do
     visit "users/sign_up"
     within("form.new_user") do
-      fill_in "Nome", with: "Ricardo Souza"
-      fill_in "Instituição", with: "UFG"
-      fill_in "Matrícula", with: "16/20481"
-      fill_in "Apelido", with: "Rick"
-      fill_in "Idade", with: "23"
+      # fill_in "Nome", with: "Ricardo Souza"
+      # fill_in "Instituição", with: "UFG"
+      # fill_in "Matrícula", with: "16/20481"
+      # fill_in "Apelido", with: "Rick"
+      # fill_in "Idade", with: "23"
       fill_in "E-mail", with: "ricardo@ufg.com.br"
       fill_in "Senha", with: "123456"
       fill_in "Confirmar Senha", with: "123456"
     end
-    click_button "Cadastrar-se"
-    expect(page).to have_content "A message with a confirmation link has been sent to your email address"
+    click_button "Cadastrar"
+    expect(page).to have_content "Por favor confirme sua conta antes de continuar."
   end
 end
